4899096d9ec9117fc979553a524c3486451fee38
niaramadhania226-max <niaramadhania226@gmail.com>
2025-08-22 22:24:27 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle navbar
+const navbarNav = document.querySelector(".navigation");
+
+document.querySelector("#hamburger-menu").onclick = (e) => {
+  navbarNav.classList.toggle("active");
+  e.preventDefault();
+};
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
